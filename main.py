84d512e6e679d4a57f0fc4f311b0ccd85f4e89ef
fractalenjoyer <92572174+fractalenjoyer@users.noreply.github.com>
2022-12-05 14:27:25 +0100

    added iter

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import mandelgen as mg
+from tkinter import *
 from PIL import Image
 import numpy as np
+from timeit import timeit
 
 dim = 1000
 
 def main():
-    # img = np.zeros((dim,dim, 3), dtype=np.uint8)
-    # for x in range(dim):
-    #     for y in range(dim):
-    #         a, b = 3*x / dim - 2, 3* y / dim - 1.5
-    #         img[x, y, 0] = mg.calc(a,b)
-    img = mg.get_mandel(dim)
-    Image.fromarray(img).save("mandelbrot.png")
+    print(timeit(lambda: mg.get_mandel(dim, 255), number=10))
+    #Image.fromarray(img).show()
 main()
--- mandelgen/src/lib.rs
+++ mandelgen/src/lib.rs
@@ -2,15 +2,9 @@ use pyo3::prelude::*;
 use numpy::ndarray::{Array3, Dim};
 use numpy::{IntoPyArray, PyArray};
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
-fn calc(a: f64, b: f64) -> i32 {
+fn calc(a: f64, b: f64, iter: usize) -> i32 {
     let (mut zr, mut zi, mut zr2, mut zi2) = (0.0, 0.0, 0.0, 0.0);
-    for i in 1..10000 {
+    for i in 1..iter {
         zi = (zi + zi) * zr + b;
         zr = zr2 - zi2 + a;
         zr2 = zr * zr;
@@ -23,14 +17,14 @@ fn calc(a: f64, b: f64) -> i32 {
 }
 
 #[pyfunction]
-fn get_mandel<'py>(py:Python<'py>, dim: usize) -> PyResult<&PyArray<u8, Dim<[usize; 3]>>> {
+fn get_mandel<'py>(py:Python<'py>, dim: usize, iter: usize) -> PyResult<&PyArray<u8, Dim<[usize; 3]>>> {
     let mut image = Array3::<u8>::zeros([dim, dim, 3]);
     let (mut a, mut b): (f64, f64);
     for x in 1..dim {
+        a = 3. * (x as f64) / (dim as f64) - 2.;
         for y in 1..dim {
-            a = 3. * (x as f64) / (dim as f64) - 2.;
             b = 3. * (y as f64) / (dim as f64) - 1.5;
-            image[[x, y, 0]] = calc(a, b) as u8;
+            image[[x, y, 0]] = calc(a, b, iter) as u8;
         }
     }
     Ok(image.into_pyarray(py))
@@ -38,7 +32,6 @@ fn get_mandel<'py>(py:Python<'py>, dim: usize) -> PyResult<&PyArray<u8, Dim<[usi
 /// A Python module implemented in Rust.
 #[pymodule]
 fn mandelgen(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(get_mandel, m)?)?;
     Ok(())
 }
