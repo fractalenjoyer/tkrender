370014928b259b617c057bf54a41df86a7756142
fractalenjoyer <92572174+fractalenjoyer@users.noreply.github.com>
2023-01-18 10:50:17 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,6 @@ impl Polygon {
         }
 
         // calculate surface normal of the polygon
-        // let normal: Vector3<f64> = points.iter().reduce(|a, b| &a.cross(b)).unwrap();
         let normal: Vector3<f64> = (b - a).cross(&(c - a));
         let norm = inv_sqrt(normal.norm_squared());
         Self {
@@ -45,16 +44,14 @@ impl Polygon {
         matrix.mul_to(&self.normal.clone(), &mut normal);
         Self { points, normal }
     }
+    
 }
 
-#[pyclass]
 struct Mesh {
     polygons: Vec<Polygon>,
 }
 
-#[pymethods]
 impl Mesh {
-    #[new]
     fn load(path: String) -> Self {
         let contents = std::fs::read_to_string(path).unwrap();
         let mut points = Vec::new();
@@ -82,6 +79,7 @@ impl Mesh {
                 _ => {}
             }
         }
+
         let polygons = faces
             .into_par_iter()
             .map(|face| {
@@ -95,7 +93,7 @@ impl Mesh {
         Self { polygons }
     }
 
-    fn rotate_in_place(&mut self, angle_x: f64, angle_y: f64, angle_z: f64) -> PyResult<()> {
+    fn rotate_in_place(&mut self, angle_x: f64, angle_y: f64, angle_z: f64) -> Result<(), ()> {
         let rotation = Rotation3::from_euler_angles(angle_x, angle_y, angle_z);
         self.polygons
             .par_iter_mut()
@@ -103,7 +101,7 @@ impl Mesh {
         Ok(())
     }
 
-    fn rotate(&self, angle_x: f64, angle_y: f64, angle_z: f64) -> PyResult<Vec<Vec<f64>>> {
+    fn rotate(&self, angle_x: f64, angle_y: f64, angle_z: f64) -> Result<Vec<Vec<f64>>, ()> {
         let rotation = Rotation3::from_euler_angles(angle_x, angle_y, angle_z);
         let polygons = self
             .polygons
@@ -119,13 +117,12 @@ impl Mesh {
         Ok(result)
     }
 
-    #[args(disable_culling = false)]
     fn get_view(
         &self,
         focal: Vec<f64>,
         origin: Vec<f64>,
         disable_culling: bool,
-    ) -> PyResult<Vec<Vec<Vec<f64>>>> {
+    ) -> Result<Vec<Vec<Vec<f64>>>, ()> {
         let origin = Vector3::new(origin[0], origin[1], origin[2]);
         // let orientation = Vector3::new(orientation[0], orientation[1], orientation[2]);
         let focal = Vector3::new(focal[0], focal[1], focal[2]);
@@ -150,14 +147,13 @@ impl Mesh {
             .collect::<Vec<Vec<Vec<f64>>>>())
     }
 
-    #[args(disable_culling = false, disable_occlusion = false)]
     fn get_shaded(
         &self,
         focal: Vec<f64>,
         origin: Vec<f64>,
         disable_culling: bool,
         disable_occlusion: bool,
-    ) -> PyResult<Vec<(Vec<Vec<f64>>, f64)>> {
+    ) -> Result<Vec<(Vec<Vec<f64>>, f64)>, ()> {
         let origin = Vector3::new(origin[0], origin[1], origin[2]);
         let focal = Vector3::new(focal[0], focal[1], focal[2]);
         let mut culled = self
@@ -186,8 +182,143 @@ impl Mesh {
             culled.sort_by_cached_key(|(poly, _)| {
                 poly.iter().map(|point| -point[2]).sum::<f64>().round() as i64
             });
-        }    
-        
+        }
+
         Ok(culled)
+    }
+
+    fn get_raw(
+        &self,
+        focal: Vec<f64>,
+        origin: Vec<f64>,
+        disable_culling: bool,
+        disable_occlusion: bool,
+    ) -> Vec<Polygon> {
+        let origin = Vector3::new(origin[0], origin[1], origin[2]);
+        let focal = Vector3::new(focal[0], focal[1], focal[2]);
+        let mut culled = self
+            .polygons
+            .par_iter()
+            .filter_map(|polygon| {
+                if polygon.normal[2] < 0.0 || disable_culling {
+                    let mut points = Vec::new();
+                    for point in &polygon.points {
+                        let mut point_view = point.clone();
+                        point_view -= &origin;
+                        point_view *= focal[2] / point_view[2];
+                        point_view += &focal;
+                        points.push(Vector3::new(point_view[0], point_view[1], point[2]));
+                    }
+                    Some(Polygon {
+                        points,
+                        normal: polygon.normal,
+                    })
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<Polygon>>();
+        if !disable_occlusion {
+            culled.sort_by_cached_key(|poly| {
+                poly.points
+                    .iter()
+                    .map(|point| -point[2])
+                    .sum::<f64>()
+                    .round() as i64
+            });
+        }
+        culled
+    }
+}
+
+#[pyclass]
+struct Renderer {
+    mesh: Mesh,
+    focal: Vec<f64>,
+    origin: Vec<f64>,
+    width: usize,
+    height: usize,
+    range: f64,
+}
+
+#[pymethods]
+impl Renderer {
+    #[new]
+    fn new(
+        path: String,
+        width: usize,
+        height: usize,
+        range: f64,
+        focal: Vec<f64>,
+        origin: Vec<f64>,
+    ) -> PyResult<Self> {
+        let mesh = Mesh::load(path);
+
+        Ok(Self {
+            mesh,
+            focal,
+            origin,
+            width,
+            height,
+            range,
+        })
+    }
+
+    fn rotate(&mut self, angle_x: f64, angle_y: f64, angle_z: f64) -> PyResult<()> {
+        match self.mesh.rotate_in_place(angle_x, angle_y, angle_z) {
+            Ok(_) => Ok(()),
+            Err(_) => Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(
+                "Invalid rotation",
+            )),
+        }
+    }
+
+    fn rasterize(&self) -> PyResult<Vec<Vec<f64>>> {
+        let mut z_buffer = vec![vec![f64::INFINITY; self.width]; self.height];
+        let mut result = vec![vec![0.0; self.width]; self.height];
+        let mut polygons = self.mesh.get_raw(
+            self.focal.clone(),
+            self.origin.clone(),
+            false,
+            false,
+        );
+
+        for polygon in &mut polygons {
+            let mut min_x = f64::INFINITY;
+            let mut max_x = f64::NEG_INFINITY;
+            let mut min_y = f64::INFINITY;
+            let mut max_y = f64::NEG_INFINITY;
+            for point in &polygon.points {
+                min_x = min_x.min(point[0]);
+                max_x = max_x.max(point[0]);
+                min_y = min_y.min(point[1]);
+                max_y = max_y.max(point[1]);
+            }
+            let min_x = (min_x / self.range * self.width as f64 / 2.0 + self.width as f64 / 2.0)
+                .round() as usize;
+            let max_x = (max_x / self.range * self.width as f64 / 2.0 + self.width as f64 / 2.0)
+                .round() as usize;
+            let min_y = (min_y / self.range * self.height as f64 / 2.0 + self.height as f64 / 2.0)
+                .round() as usize;
+            let max_y = (max_y / self.range * self.height as f64 / 2.0 + self.height as f64 / 2.0)
+                .round() as usize;
+            for x in min_x..max_x {
+                for y in min_y..max_y {
+                    let point = Vector3::new(
+                        (x as f64 - self.width as f64 / 2.0) / self.width as f64 * 2.0 * self.range,
+                        (y as f64 - self.height as f64 / 2.0) / self.height as f64 * 2.0 * self.range,
+                        0.0,
+                    );
+                    if polygon.contains(&point) {
+                        let z = polygon.get_z(&point);
+                        if z < z_buffer[y][x] {
+                            z_buffer[y][x] = z;
+                            result[y][x] = polygon.normal.dot(&Vector3::new(0.0, 0.0, 1.0)).abs();
+                        }
+                    }
+                }
+            }
+        }
+        Ok(result)
     }
 }
